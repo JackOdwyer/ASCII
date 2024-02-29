22c01ba713ae1e5d13050083fe52ef7f1d866853
Draunt <100187574+JackOdwyer@users.noreply.github.com>
2024-03-01 00:45:30 +1100

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,19 @@
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    var clock = 
+        // Hours
+        (hours < 10 ? "0" : "") + hours + ":" +
+        // Minutes
+        (minutes < 10 ? "0" : "") + minutes + ":" +
+        // Seconds
+        (seconds < 10 ? "0" : "") + seconds;
+
+    document.getElementById("clock").innerText = clock;
+}
+
+setInterval(updateClock, 1000);
 
